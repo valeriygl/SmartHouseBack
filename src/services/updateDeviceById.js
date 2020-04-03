1c71fc475ffd6fd72e982aa6a80af5530f28364f
valeriygl <39625852+valeriygl@users.noreply.github.com>
2020-04-03 17:48:24 +0300

    Revert "Features/search"

--- src/services/updateDeviceById.js
+++
@@ -1,9 +0,0 @@
-const updateDeviceById = (devices, id, newDevice) => {
-  const updatedDevices = devices.map(device =>
-    device.id === id ? { ...newDevice, id } : device
-  );
-
-  return updatedDevices;
-};
-
-module.exports = updateDeviceById;
