555cf289811356931089383adf8ab818a6029f4b
Denys Voskoboinykov <denisvoskoboinykov@gmail.com>
2020-04-03 11:50:38 +0300

    added delete device controller

---
+++ src/controllers/index.js
@@ -0,0 +1,5 @@
+const deleteDevice = require('./deleteDevice');
+
+module.exports = {
+  deleteDevice
+};
---
+++ src/services/index.js
@@ -0,0 +1,9 @@
+const deleteDeviceById = require('./deleteDeviceById');
+const readFile = require('./readFile');
+const writeFile = require('./writeFile');
+
+module.exports = {
+  deleteDeviceById,
+  readFile,
+  writeFile
+};
