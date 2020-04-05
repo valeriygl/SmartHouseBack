ec074764ac9141553565d93b4a2a9249e3846ca6
valeriygl <glyancevval@gmail.com>
2020-04-05 22:42:43 +0300

    cleaned

--- src/controllers/isHomeExist.js
+++ src/controllers/isHomeExist.js
@@ -3,9 +3,13 @@ const { storePath } = require('../config');
 
 const isHomeExist = async (req, res, next) => {
   const id = Number(req.params.homeid);
+
   const store = await readFile(storePath);
+
   const homes = JSON.parse(store);
+
   const isExist = homes.find(home => home.id === id);
+
   if (isExist) {
     next();
   } else {
