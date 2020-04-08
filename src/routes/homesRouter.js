86f65fa9ab7a4be603f36fe02dc60f2f842b36f0
valeriygl <glyancevval@gmail.com>
2020-04-08 09:49:57 +0300

    added validation schema

--- src/routes/homesRouter.js
+++ src/routes/homesRouter.js
@@ -1,7 +1,8 @@
 const app = require('express');
 const bodyParser = require('body-parser');
 const errorHandler = require('errorhandler');
-const { postHome, getHome } = require('../controllers');
+const { postHome, getHome, validationMiddleware } = require('../controllers');
+const { validationSchema } = require('../services');
 const { updateHome, deleteHome, getAllHomes } = require('../controllers/home');
 
 const homesRouter = app.Router();
@@ -10,10 +11,20 @@ const jsonParser = bodyParser.json();
 
 homesRouter.use(errorHandler());
 
-homesRouter.post('/', jsonParser, postHome);
+homesRouter.post(
+  '/',
+  jsonParser,
+  validationMiddleware(validationSchema.homePOST),
+  postHome
+);
 homesRouter.get('/', getAllHomes);
 homesRouter.get('/:homeid', getHome);
 homesRouter.delete('/:homeid', deleteHome);
-homesRouter.put('/:homeid', jsonParser, updateHome);
+homesRouter.put(
+  '/:homeid',
+  jsonParser,
+  validationMiddleware(validationSchema.homePUT),
+  updateHome
+);
 
 module.exports = homesRouter;
