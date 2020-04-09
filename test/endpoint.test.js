27d0668b85823aa89239c3ea33abda0c165bbcb8
valeriygl <glyancevval@gmail.com>
2020-04-09 19:38:52 +0300

    added endpoint tests

---
+++ test/endpoint.test.js
@@ -0,0 +1,125 @@
+const request = require('supertest');
+const app = require('../src/app');
+describe('Endpoints', () => {
+  it('should respond with 422 status code because validation error', async () => {
+    const res = await request(app).post('/api/homes').send({
+      name: 'third',
+      devices: [],
+      sad: 'sd',
+    });
+    expect(res.statusCode).toEqual(422);
+  });
+  it('should create a new post', async () => {
+    const res = await request(app).post('/api/homes').send({
+      name: 'third',
+      devices: [],
+    });
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty('id');
+  });
+  it('should get 1 item', async () => {
+    const res = await request(app).get('/api/homes/2');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty('id');
+  });
+  it('should get all items', async () => {
+    const res = await request(app).get('/api/homes');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveLength(2);
+  });
+  it('should update a item', async () => {
+    const res = await request(app).put('/api/homes/2').send({
+      name: 'good',
+    });
+
+    expect(res.statusCode).toEqual(200);
+  });
+  it('should delete a post', async () => {
+    const res = await request(app).delete('/api/homes/2');
+    expect(res.statusCode).toEqual(200);
+  });
+
+  it('should respond with status code 404 if resource is not found', async () => {
+    const res = await request(app).get('/api/homes/2');
+    expect(res.statusCode).toEqual(404);
+  });
+  // sd
+  it('should respond with 422 status code because validation error', async () => {
+    const res = await request(app)
+      .post('/api/homes/1/devices')
+      .send({
+        name: 'Oven',
+        status: true,
+        type: 'oven',
+        image: '',
+        temp: {
+          max: 0,
+          current: 0,
+          step: 0,
+        },
+        modes: [],
+        currentMode: '',
+      });
+    expect(res.statusCode).toEqual(422);
+  });
+  it('should create a new post', async () => {
+    const res = await request(app)
+      .post('/api/homes/1/devices')
+      .send({
+        name: 'Oven',
+        status: true,
+        type: 'oven',
+        image: '',
+        temp: {
+          min: 0,
+          max: 0,
+          current: 0,
+          step: 0,
+        },
+        modes: [],
+        currentMode: '',
+      });
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty('id');
+  });
+  it('should get 1 item', async () => {
+    const res = await request(app).get('/api/homes/1/devices/1');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty('id');
+  });
+  it('should get all items', async () => {
+    const res = await request(app).get('/api/homes/1/devices');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty('data');
+    expect(res.body).toHaveProperty('page');
+  });
+  it('should update a item', async () => {
+    const res = await request(app)
+      .put('/api/homes/1/devices/1')
+      .send({
+        name: 'Oven',
+        status: false,
+        type: 'oven',
+        image: '',
+        temp: {
+          min: 0,
+          max: 0,
+          current: 0,
+          step: 0,
+        },
+        modes: [],
+        currentMode: '',
+      });
+
+    expect(res.statusCode).toEqual(200);
+  });
+  it('should delete a post', async () => {
+    const res = await request(app).delete('/api/homes/1/devices/1');
+    expect(res.statusCode).toEqual(200);
+  });
+
+  it('should respond with status code 404 if resource is not found', async () => {
+    const res = await request(app).get('/api/homes/1/devices/1');
+    expect(res.statusCode).toEqual(404);
+  });
+});
